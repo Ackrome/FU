8596bb66d585f176c28e50a47311b5ba2fe59114
Ackrome <ivantishchenko59@gmail.com>
2025-10-13 23:37:09 +0300

    что-то глупенькое

--- Course_3/DL/screenshot_script.py
+++ Course_3/DL/screenshot_script.py
@@ -2,7 +2,7 @@ import sys
 import time
 from playwright.sync_api import sync_playwright
 
-def take_screenshot(url, output_path, view_size={"width": 1920, "height": 1480}):
+def take_screenshot(url, output_path, width=1920, height=1480):
     """
     Launches a browser, navigates to a URL, and saves a screenshot.
     This function is designed to be called from the command line.
@@ -11,7 +11,7 @@ def take_screenshot(url, output_path, view_size={"width": 1920, "height": 1480})
         with sync_playwright() as p:
             browser = p.chromium.launch()
             page = browser.new_page()
-            page.set_viewport_size(view_size)
+            page.set_viewport_size({"width": width, "height": height})
             page.goto(url)
             time.sleep(5)
             page.screenshot(path=output_path)
@@ -22,11 +22,13 @@ def take_screenshot(url, output_path, view_size={"width": 1920, "height": 1480})
         sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) <=2 :
-        print("Usage: python screenshot_script.py <URL> <output_path>", file=sys.stderr)
+    if len(sys.argv) !=5 :
+        print("Usage: python screenshot_script.py <URL> <output_path> [width] [height]", file=sys.stderr)
         sys.exit(1)
     
     url_arg = sys.argv[1]
     path_arg = sys.argv[2]
+    width_arg = sys.argv[3]
+    height_arg = sys.argv[4]
     
-    take_screenshot(url_arg, path_arg)
+    take_screenshot(url_arg, path_arg, width_arg, height_arg)
